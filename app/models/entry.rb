c082f319328cfe4acc0c92f96e4730caa0fa198b
Clayton Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2023-04-03 19:40:15 -0600

    update the entry model to pass its associated rspec tests

---
+++ app/models/entry.rb
@@ -0,0 +1,23 @@
+class Entry < ApplicationRecord
+  belongs_to :journal
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :published_at, presence: true
+  validate :published_at_cannot_be_in_the_future
+
+  scope :published, -> { where(is_published: true) }
+
+  def published!
+    self.is_published = true
+    self.save
+  end
+
+  private
+
+  def published_at_cannot_be_in_the_future
+    if published_at.present? && published_at > Time.now
+      errors.add(:published_at, "can't be in the future")
+    end
+  end
+end
